99614cd3448e3bf3fffefb7783e14d6570f3b418
ntlong <ntlong@humaxdigital.com>
2018-01-09 17:04:09 +0700

    possible to bring up with order

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var options = {
         collapse_vars : true
     }
 }
+
 /*scripts:development
 * minify: disable
 * souremaps: enable
@@ -31,11 +32,13 @@ gulp.task('scripts:development', function() {
 
     /*in this scenario use uglify-js() which is much more powerfull with option */
     pump([
-        gulp.src('./src/script/*.js'),
-        // sourcemaps.init(),
+        gulp.src(['./src/vendor/jquery.min.js',
+        './src/vendor/angular.min.js',
+        './src/script/*.js']), // try to control the order
+        sourcemaps.init(),
         concat('app.js'),
         minify(options),
-        // sourcemaps.write(),
+        sourcemaps.write(),
         gulp.dest('./dist/')
     ]);
 });
@@ -81,8 +84,8 @@ gulp.task('clean', function() {
     return del(['dist/*']);
 });
 
-gulp.task('default', ['scripts:development', 'html', 'sass', 'watch:development']);
-gulp.task('release', ['scripts:release', 'html', 'sass', 'watch:release']);
+gulp.task('default', ['clean','scripts:development', 'html', 'sass', 'watch:development']);
+gulp.task('release', ['clean','scripts:release', 'html', 'sass', 'watch:release']);
 /*
 * there is no source map at this time
 * install gulp-sourcemaps
