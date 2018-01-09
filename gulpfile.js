28183e673c6d6a3fd325d3bdfc8cfa0a71440c20
ntlong <ntlong@humaxdigital.com>
2018-01-09 15:18:37 +0700

    replace minify() by uglify()

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,40 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifyjs = require('gulp-js-minify');
 var sourcemaps = require('gulp-sourcemaps');
-
+var pump = require('pump');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var uglifyjs = require("uglify-js");
+var composer = require('gulp-uglify/composer');
+var minify = composer(uglifyjs, console);
+var options = {
+    compress : {
+        collapse_vars : true
+    }
+}
 /*scripts:development
 * minify: disable
 * souremaps: enable
 * concat: enable
 */
 gulp.task('scripts:development', function() {
-    return gulp.src('./src/script/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(concat('app.js'))
-        .pipe(sourcemaps.write())
-        //.pipe(minifyjs())
-        .pipe(gulp.dest('./dist/'));
+    /*in this scenario use minifijs() */
+    // return gulp.src('./src/script/*.js')
+    //     .pipe(sourcemaps.init())
+    //     .pipe(concat('app.js'))
+    //     .pipe(sourcemaps.write())
+    //     //.pipe(minifyjs())
+    //     .pipe(gulp.dest('./dist/'));
+
+    /*in this scenario use uglify-js() which is much more powerfull with option */
+    pump([
+        gulp.src('./src/script/*.js'),
+        // sourcemaps.init(),
+        concat('app.js'),
+        minify(options),
+        // sourcemaps.write(),
+        gulp.dest('./dist/')
+    ]);
 });
 
 /*scripts:release
@@ -28,7 +49,7 @@ gulp.task('scripts:release', function() {
     return gulp.src('./src/script/*.js')
         .pipe(concat('app.js'))
         .pipe(minifyjs())
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('./dist/*'));
 });
 
 
@@ -56,9 +77,12 @@ gulp.task('watch:release', function () {
     gulp.watch('./src/*.html', ['html']);
 });
 
-gulp.task('development', ['scripts:development', 'html', 'sass', 'watch:development']);
-gulp.task('release', ['scripts:release', 'html', 'sass', 'watch:release']);
+gulp.task('clean', function() {
+    return del(['dist/*']);
+});
 
+gulp.task('default', ['scripts:development', 'html', 'sass', 'watch:development']);
+gulp.task('release', ['scripts:release', 'html', 'sass', 'watch:release']);
 /*
 * there is no source map at this time
 * install gulp-sourcemaps
--- src/script/main.js
+++ src/script/main.js
@@ -1,2 +1,11 @@
 console.log("hello from main");
 console.log("not sure this has source map or not");
+// this time I use uglify
+var long_varialbename = "jacky";
+console.log(long_varialbename);
+
+function jacky () {
+    var local_var = 1;
+    local_var++;
+    console.log(local_var);
+}
